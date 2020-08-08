7d9f46f546b17ed715f106bd5c143b2f3fe59102
takeru1205 <takeru0639418@gmail.com>
2020-08-08 20:43:22 +0900

    [add] noise variables

--- const.py
+++ const.py
@@ -5,3 +5,6 @@ weight_decay = 1e-2  # L2 weight decay
 actor_lr = 3e-4  # learning rate for actor network
 critic_lr = 3e-4  # learning rate for critic network
 tau = 0.005  # soft target update coefficient
+policy_noise = 0.2
+noise_clip = 0.5
+policy_freq = 2  # Delayed policy update
