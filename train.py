df55b357ecbb3132ce1883c21fa682a15d3933cc
takeru1205 <takeru0639418@gmail.com>
2020-08-08 17:32:02 +0900

    [update] get action

--- train.py
+++ train.py
@@ -12,28 +12,34 @@ env.seed(42)
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
 
-# writer = SummaryWriter(log_dir='logs/')
+writer = SummaryWriter(log_dir='logs/')
 
-agent = TD3(env)
+agent = TD3(env, writer=writer)
 
-epoch = 500
+epoch = 150
+initial_act = 1000
 all_timestep = 0
 
 for e in range(epoch):
     state = env.reset()
     cumulative_reward = 0
     for i in range(200):
-        action = agent.get_action(state)
-        state_, reward, done, _ = env.step(action * env.action_space.high[0])
+        if all_timestep < initial_act:
+            action = agent.get_action(state, initial_act=True)
+            state_, reward, done, _ = env.step(action)
+        else:
+            action = agent.get_action(state)
+            state_, reward, done, _ = env.step(action * env.action_space.high[0])
+        env.render()
         agent.store_transition(state, action, state_, reward, done)
 
         state = state_
         cumulative_reward += reward
 
-        agent.update()
+        agent.update(all_timestep)
         all_timestep += 1
     print('Epoch : {} / {}, Cumulative Reward : {}'.format(e, epoch, cumulative_reward))
-    # writer.add_scalar("reward", cumulative_reward, e)
+    writer.add_scalar("reward", cumulative_reward, e)
 
 agent.save_model()
 env.close()
